eb9c39382cf6cf88c79d53a043749a319cf21edf
Mitchell Horne <Mitchell.Horne@addynamo.com>
2016-09-29 12:07:05 +0200

    Add heading state

--- src/js/accoridian.js
+++ src/js/accoridian.js
@@ -1,9 +1,14 @@
+//Get all the headers that can be clicked
 const service = $('.service-container-header');
 
 service.on('click', (event)=>{
+    //Moving the currently active header to latest clicked one
+    $('.service-container-header.active').removeClass('active');
+    event.currentTarget.className += ' active';
+
+    //Open the closed service container body
     const serviceBody       = $('.' + event.currentTarget.dataset.serviceBody);
     const activeServiceBody = $('.active-service-body');
-
 
     if(serviceBody[0] === activeServiceBody[0]) {
         serviceBody.removeClass('active-service-body');

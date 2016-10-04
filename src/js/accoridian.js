2ffff56bcea6a3c4aa3d7c1ba56577d261ad3e05
hornemitch <mitchhorne92@gmail.com>
2016-10-04 20:32:58 +0200

    Got up to date

--- src/js/accoridian.js
+++ src/js/accoridian.js
@@ -5,12 +5,18 @@ service.on('click', (event)=>{
     const serviceHeader = event.currentTarget;
     $('.service-container-header.active').removeClass('active');
 
+    const thisServiceId     = event.currentTarget.id;
     const serviceBody       = $('.' + event.currentTarget.dataset.serviceBody);
     const activeServiceBody = $('.active-service-body');
 
     if(serviceBody[0] === activeServiceBody[0]) {
         serviceBody.removeClass('active-service-body');
         serviceBody.addClass('closed');
+
+        ($('html, body').animate({
+            scrollTop: $("#service-id-1").offset().top
+        }, 500));
+
         return;
     }
 
@@ -22,10 +28,17 @@ service.on('click', (event)=>{
         setTimeout(()=>{
             serviceBody.addClass('active-service-body');
             serviceBody.removeClass('closed');
+            ($('html, body').animate({
+                scrollTop: $("#" + thisServiceId).offset().top
+            }, 500));
         }, 500);
     } else {
         serviceHeader.className += ' active';
         serviceBody.addClass('active-service-body');
         serviceBody.removeClass('closed');
+
+        ($('html, body').animate({
+            scrollTop: $("#" + thisServiceId).offset().top
+        }, 500));
     }
 });
